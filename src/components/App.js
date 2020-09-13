f18180516d143546c68619fe497b14f901ee5ffc
Meredith Gallop <meredith@trtl.co.uk>
2020-09-13 15:02:06 +0100

    inital commit

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+
+
+const PageOne = () => {
+    return <div>
+        PageOne
+        <Link to="/pagetwo">Navgate to pg 2</Link>
+    </div>
+}
+
+const PageTwo = () => {
+    return <div>
+        PageTwo
+        <button>Click Me!</button>
+        <Link to="/">Navgate to pg 1</Link>
+    </div>
+}
+
+const App = () => {
+        return (
+            <div>
+                <BrowserRouter>
+                <div>
+                <Route path="/" exact component={PageOne} />
+                <Route path="/pagetwo" component={PageTwo} />
+                </div>
+                </BrowserRouter>
+            </div>
+        )
+}
+
+export default App;
