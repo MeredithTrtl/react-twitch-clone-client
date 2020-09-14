3206d8ebc5b1beee29e506c01af6e1516aa40292
Meredith Gallop <meredith@trtl.co.uk>
2020-09-14 20:14:47 +0100

    header setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,25 @@
 import React from 'react'
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 
+import StreamCreate from './Streams/StreamCreate';
+import StreamEdit from './Streams/StreamEdit';
+import StreamDelete from './Streams/StreamDelete';
+import StreamList from './Streams/StreamList';
+import StreamShow from './Streams/StreamShow';
 
-const PageOne = () => {
-    return <div>
-        PageOne
-        <Link to="/pagetwo">Navgate to pg 2</Link>
-    </div>
-}
-
-const PageTwo = () => {
-    return <div>
-        PageTwo
-        <button>Click Me!</button>
-        <Link to="/">Navgate to pg 1</Link>
-    </div>
-}
+import Header from './Header'
 
 const App = () => {
         return (
-            <div>
+            <div className="ui container">
                 <BrowserRouter>
                 <div>
-                <Route path="/" exact component={PageOne} />
-                <Route path="/pagetwo" component={PageTwo} />
+                <Header />
+                    <Route path='/' exact component={StreamList}/>
+                    <Route path='/streams/new' exact component={StreamCreate}/>
+                    <Route path='/streams/edit' exact component={StreamEdit}/>
+                    <Route path='/streams/delete' exact component={StreamDelete}/>
+                    <Route path='/streams/show' exact component={StreamShow}/>
                 </div>
                 </BrowserRouter>
             </div>
